b4b8739fe0dcc71bd2b3c025e5a079666a8f0015
Juliana Nascimento <91079930+junascimento9@users.noreply.github.com>
2023-06-09 17:49:49 +0000

    commit 2

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '6CcDPvaf5JlVv1xsCyAE6FSDWGXXG8kLt3PmcBM1v0FrOYcmlw'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@localhost/registro_alunos'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
--- wsgi.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from .routes import app_blueprint
-from .config import app, db
-
-app.register_blueprint(app_blueprint)
-
-if __name__ == '__main__':
-    app.run(debug=True)
